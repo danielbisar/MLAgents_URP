ad85bf034b5657f37438e1988ddf7ea729081049
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2023-08-16 15:50:20 +0200

    Resolve compatibility problems with change python version in ubuntu upstream repos: manual installation of python3.9

--- Scripts/ml-agents-docker/scripts/first_start.sh
+++ Scripts/ml-agents-docker/scripts/first_start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if [[ -f /is_initialized ]]; then
+set -e
+
+mkdir -p /container_state
+
+if [[ -f /container_state/is_initialized ]]; then
     exit 0
 fi
 
-echo "Setup password for user:"
-passwd mlagents
+if [[ ! -f /container_state/pwd_set ]]; then
+    echo "Setup password for user 'mlagents':"
+    passwd mlagents
+    touch /container_state/pwd_set
+fi
 
 sudo -u mlagents /tmp/scripts/clone_mlagents.sh
 sudo -u mlagents /bin/bash -c /tmp/scripts/setup_venv.sh
+
+touch /container_state/is_initialized
--- Scripts/ml-agents-docker/scripts/setup_venv.sh
+++ Scripts/ml-agents-docker/scripts/setup_venv.sh
@@ -9,16 +9,20 @@ if [ -d ~/src/venv ]; then
     rm -rf ~/src/venv
 fi
 
-python3 -m venv venv
+python3.9 -m venv venv
 . ~/src/venv/bin/activate
-pip install pip --upgrade
 
+mkdir -p /tmp/pip_install && cd /tmp/pip_install
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+
+# requires Python Version >=3.8.13,<=3.10.8;
 pip install mlagents==0.30.0
 # will also install
 # Collecting torch<=1.11.0,>=1.8.0 (from mlagents==0.30.0)
 #   Downloading torch-1.11.0-cp310-cp310-manylinux1_x86_64.whl (750.6 MB)
 
-# fix protobuf version; 4.23.2 will due to an incorrect dependencey statement saying protobuf>=3.6
+# fix protobuf version; 4.23.2 which was installed, due to an incorrect dependency statement (protobuf>=3.6)
 # mlagents does not work with that, so we downgrade to a working version
 pip install protobuf~=3.20.0 --upgrade
 
